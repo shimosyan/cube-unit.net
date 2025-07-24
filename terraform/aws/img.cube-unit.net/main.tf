29ae705816d2cb584b9e34d04dd70f3e9813d5e3
shimosyan <owner@cube-unit.net>
2025-07-25 00:02:34 +0900

    feat: Terraform構成とGitHub Actionsワークフローの追加
    - .gitignoreにTerraform関連のファイルを追加
    - 辞書ファイルを.configディレクトリに追加
    - terraform-deploy.yamlとterrafotm-pull-request.yamlにGitHub Actionsのワークフローを追加
    - aqua.yamlにTerraformと関連ツールのパッケージを追加
    - cspell.jsonに辞書ファイルのパスを設定
    - AWS用のmain.tfファイルを追加し、S3バックエンドを設定
    - tfaction.yamlを追加し、Terraformのターゲット設定を行う
    - tfaction-root.yamlに複数のターゲットグループを設定

---
+++ terraform/aws/img.cube-unit.net/main.tf
@@ -0,0 +1,28 @@
+/*
+  terraform 定義
+*/
+terraform {
+  required_version = ">= 1.5.0"
+
+  /*
+    .tfstate は AWS S3 に保存すること
+  */
+  backend "s3" {
+    bucket  = "terraform.cube-unit.net"
+    region  = "ap-northeast-1"
+    key     = "cube-unit.net/aws/img.cube-unit.net/terraform.tfstate"
+    encrypt = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-northeast-1"
+}
