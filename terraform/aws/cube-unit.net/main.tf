5aeb755d91a465ff5d96fff10856f96c37e7aa0d
shimosyan <owner@cube-unit.net>
2025-07-26 04:20:14 +0900

    feat: cube-unit.netおよびstg.cube-unit.net用のCloudFrontおよびS3リソースを追加 (#8)
    - cube-unit.netおよびstg.cube-unit.net用のCloudFrontおよびS3リソースを追加
    - ACM証明書の設定をus-east-1リージョンに指定
    - S3バケットの公開アクセスブロックとポリシーを設定
    - tfaction.yamlおよびtfaction-root.yamlを更新
    - .terraform.lock.hclを生成

---
+++ terraform/aws/cube-unit.net/main.tf
@@ -0,0 +1,35 @@
+/*
+  terraform 定義
+*/
+terraform {
+  required_version = ">= 1.5.0"
+
+  /*
+    .tfstate は AWS S3 に保存すること
+  */
+  backend "s3" {
+    bucket  = "terraform.cube-unit.net"
+    region  = "ap-northeast-1"
+    key     = "cube-unit.net/aws/cube-unit.net/terraform.tfstate"
+    encrypt = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = local.region
+}
+
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
+
+data "aws_caller_identity" "current" {}
