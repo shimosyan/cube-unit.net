8227f08d0f808c3294bcabb6117170124a3d970f
shimosyan <owner@cube-unit.net>
2025-07-26 04:43:42 +0900

    fix: locals.tfのドメイン定義のスペースを修正
    - terraform/aws/cube-unit.net/locals.tf: ドメイン定義にスペースを追加
    - terraform/aws/img.cube-unit.net/locals.tf: ドメイン定義にスペースを追加
    - terraform/aws/static.cube-unit.net/locals.tf: ドメイン定義にスペースを追加
    - terraform/aws/stg.cube-unit.net/locals.tf: ドメイン定義にスペースを追加

--- terraform/aws/img.cube-unit.net/locals.tf
+++ terraform/aws/img.cube-unit.net/locals.tf
@@ -2,5 +2,5 @@ locals {
   account_id = data.aws_caller_identity.current.account_id
   region = "ap-northeast-1"
 
-  domain="img.cube-unit.net"
+  domain = "img.cube-unit.net"
 }
--- terraform/aws/stg.cube-unit.net/locals.tf
+++ terraform/aws/stg.cube-unit.net/locals.tf
@@ -2,5 +2,5 @@ locals {
   account_id = data.aws_caller_identity.current.account_id
   region = "ap-northeast-1"
 
-  domain="stg.cube-unit.net"
+  domain = "stg.cube-unit.net"
 }
