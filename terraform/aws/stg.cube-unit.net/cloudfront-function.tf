e160426a89fcc14c77fb9491f968e5c1471f85bc
shimosyan <owner@cube-unit.net>
2025-07-26 15:07:52 +0900

    fix: URLリライト機能のドメイン名のフォーマットを修正
    - cloudfront-function.tfのname属性を修正し、ドメイン名のピリオドをハイフンに置き換えるように変更しました。

--- terraform/aws/stg.cube-unit.net/cloudfront-function.tf
+++ terraform/aws/stg.cube-unit.net/cloudfront-function.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudfront_function" "url_rewrite" {
-  name    = "${local.domain}-url-rewrite"
+  name    = "${replace(local.domain, ".", "-")}-url-rewrite"
   runtime = "cloudfront-js-1.0"
   comment = "URL rewrite function for cube-unit.net Hugo static site"
   publish = true
