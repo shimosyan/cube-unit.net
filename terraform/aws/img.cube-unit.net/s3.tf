dc1fa4b1a8e82e39401d12fc7343777c1d094e73
shimosyan <owner@cube-unit.net>
2025-08-05 01:18:27 +0900

    Fix-Trivy-error (#55)
    * fix: 差分検出のため regionのインデントを修正
    
    - terraform/aws/cube-unit.net/locals.tf: regionのインデントを修正
    - terraform/aws/img.cube-unit.net/locals.tf: regionのインデントを修正
    - terraform/aws/static.cube-unit.net/locals.tf: regionのインデントを修正
    - terraform/aws/stg.cube-unit.net/locals.tf: regionのインデントを修正
    
    * fix: CloudFrontの関数ARNを更新
    
    cloudfront.tfファイルのviewer-requestイベントに関連するfunction_arnを、ハードコーディングされたARNからaws_cloudfront_function.asset_redirect.arnに変更しました。
    
    * feat: S3バケットの暗号化とバージョン管理を追加
    
    - S3バケットにサーバーサイド暗号化を追加し、AES256アルゴリズムを使用
    - S3バケットのバージョン管理を有効化
    - .trivyignoreファイルを新規作成し、関連する警告を追加
    
    * feat: .trivyignoreファイルを追加
    
    - terraform/aws/cube-unit.net/.trivyignore: CloudFrontおよびS3バケットのログに関するコスト警告を追加
    - terraform/aws/img.cube-unit.net/.trivyignore: CloudFrontおよびS3バケットのログに関するコスト警告を追加
    
    * fix(tflint): auto fix
    
    * fix(tflint): auto fix
    
    * feat: .trivyignoreファイルの更新と追加
    
    - 既存の.trivyignoreファイルのコメントを修正し、見出しを「#」から「##」に変更しました。
    - 新たにstatic.cube-unit.netとstg.cube-unit.netの.trivyignoreファイルを追加しました。
    
    * Merge branch 'fix-Trivy-error' of https://github.com/shimosyan/cube-unit.net into fix-Trivy-error
    
    * fix(tflint): auto fix
    
    * fix(tflint): auto fix
    
    * Merge branch 'main' into fix-Trivy-error

--- terraform/aws/img.cube-unit.net/s3.tf
+++ terraform/aws/img.cube-unit.net/s3.tf
@@ -3,6 +3,25 @@ resource "aws_s3_bucket" "main" {
   region = local.region
 }
 
+# S3 bucket encryption (AVD-AWS-0088)
+resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# S3 bucket versioning (AVD-AWS-0090)
+resource "aws_s3_bucket_versioning" "main" {
+  bucket = aws_s3_bucket.main.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "main" {
   bucket = aws_s3_bucket.main.id
 
--- terraform/aws/stg.cube-unit.net/main.tf
+++ terraform/aws/stg.cube-unit.net/main.tf
@@ -32,4 +32,3 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_caller_identity" "current" {}
