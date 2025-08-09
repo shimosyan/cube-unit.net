ad785734c95d3e8b15d13c8730b535f6aea87ac6
shimosyan <owner@cube-unit.net>
2025-08-09 22:46:47 +0900

    Add_dns_micmnis.net (#61)
    * feat: Cloudflare DNS設定を追加
    
    - .terraform.lock.hcl: Cloudflareプロバイダーのバージョンを追加
    - dns-a.tf: 複数のAレコードを追加
    - dns-cname.tf: CNAMEレコードを追加
    - dns-mx.tf: GoogleのMXレコードを追加
    - dns-txt.tf: SPF、DKIM、DMARC、Googleサイト検証用のTXTレコードを追加
    - import.tf: 既存のDNSレコードのインポート設定を追加
    - locals.tf: ドメインとゾーンのローカル変数を定義
    - main.tf: Terraformの設定とバックエンドを追加
    - tfaction.yaml: 空のtfaction設定ファイルを追加
    - tfaction-root.yaml: micmnis.net用のターゲットグループを追加
    
    * refactor: 不要な空行を削除
    
    terraform/cloudflare/cube-unit.net/dns/dns-cname.tfの79行目の空行を削除しました。
    
    * chore: update .terraform.lock.hcl
    
    * style: terraform fmt -recursive

---
+++ terraform/cloudflare/micmnis.net/dns/import.tf
@@ -0,0 +1,104 @@
+import {
+  id = "785e8c4ddd38cf2e4058ce71f63310ad/4615e3fc3e5fdaedd32873beb17d9d99"
+  to = cloudflare_dns_record.grafana_a
+}
+
+import {
+  id = "785e8c4ddd38cf2e4058ce71f63310ad/ef936a242ef57202e54ad7cdef0afb33"
+  to = cloudflare_dns_record.img_a
+}
+
+import {
+  id = "785e8c4ddd38cf2e4058ce71f63310ad/1487b13ada7b8cc5ce7a2da5e15a9460"
+  to = cloudflare_dns_record.__a
+}
+
+import {
+  id = "785e8c4ddd38cf2e4058ce71f63310ad/f763b36ba1064fb323bf01ebeaadaf8c"
+  to = cloudflare_dns_record.minecraft-game_a
+}
+
+import {
+  id = "785e8c4ddd38cf2e4058ce71f63310ad/eb41cb2570e9a26c8ed1d7297061cb4d"
+  to = cloudflare_dns_record.minecraft_a
+}
+
+import {
+  id = "785e8c4ddd38cf2e4058ce71f63310ad/88fcad583aaff3175542c787b2b50e90"
+  to = cloudflare_dns_record.nas_a
+}
+
+import {
+  id = "785e8c4ddd38cf2e4058ce71f63310ad/02905c89b230bd509bdd68066335e679"
+  to = cloudflare_dns_record.proxmox_a
+}
+
+import {
+  id = "785e8c4ddd38cf2e4058ce71f63310ad/62b0da973d044d18321487e0ba2e0448"
+  to = cloudflare_dns_record.static_a
+}
+
+import {
+  id = "785e8c4ddd38cf2e4058ce71f63310ad/d13668b06231929b31f7bf4eca0034f0"
+  to = cloudflare_dns_record.vscode_a
+}
+
+import {
+  id = "785e8c4ddd38cf2e4058ce71f63310ad/05cd18c1273463a112c0aff76bf2791d"
+  to = cloudflare_dns_record._4013b3d0ef77fb56b06e8c7cb3607edc_www_cname
+}
+
+import {
+  id = "785e8c4ddd38cf2e4058ce71f63310ad/99ce300ca4f5ffe19f1024e4d60a58d8"
+  to = cloudflare_dns_record._7583d4fb40b603f3f950799759eb10a1_cname
+}
+
+import {
+  id = "785e8c4ddd38cf2e4058ce71f63310ad/a22b1db0f29dbb811a5811c8e5276f5e"
+  to = cloudflare_dns_record._google-alt4_mx
+}
+
+import {
+  id = "785e8c4ddd38cf2e4058ce71f63310ad/892149e3e5cdc21592b1e5cf290ff6d8"
+  to = cloudflare_dns_record._google-alt3_mx
+}
+
+import {
+  id = "785e8c4ddd38cf2e4058ce71f63310ad/25c853040d7933801dd2d2a1edc90ba6"
+  to = cloudflare_dns_record._google-alt2_mx
+}
+
+import {
+  id = "785e8c4ddd38cf2e4058ce71f63310ad/6fd73436d4faf0a4814653d74cea8203"
+  to = cloudflare_dns_record._google-alt1_mx
+}
+
+import {
+  id = "785e8c4ddd38cf2e4058ce71f63310ad/993c230f2aacb77cd40a7eca73d03f07"
+  to = cloudflare_dns_record._google_mx
+}
+
+import {
+  id = "785e8c4ddd38cf2e4058ce71f63310ad/a9a848eb6febae7a30ce805191966c28"
+  to = cloudflare_dns_record._dmarc_txt
+}
+
+import {
+  id = "785e8c4ddd38cf2e4058ce71f63310ad/7061b7394dd30ab5ae76c298f5e623da"
+  to = cloudflare_dns_record.google__domainkey_txt
+}
+
+import {
+  id = "785e8c4ddd38cf2e4058ce71f63310ad/ac8d2d45e16d00c72dd4a8c998531bda"
+  to = cloudflare_dns_record._google-site-verification-1_txt
+}
+
+import {
+  id = "785e8c4ddd38cf2e4058ce71f63310ad/030d85574c9be770a3508a34ddc382fa"
+  to = cloudflare_dns_record._google-site-verification-2_txt
+}
+
+import {
+  id = "785e8c4ddd38cf2e4058ce71f63310ad/b53ad0280376bce2c28d70b68fe05973"
+  to = cloudflare_dns_record._spf_txt
+}
---
+++ terraform/cloudflare/micmnis.net/dns/locals.tf
@@ -0,0 +1,4 @@
+locals {
+  domain = "micmnis.net"
+  zone   = "785e8c4ddd38cf2e4058ce71f63310ad"
+}
