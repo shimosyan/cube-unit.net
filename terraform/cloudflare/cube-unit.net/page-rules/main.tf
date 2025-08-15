e2d1f7a6c9df7f7e29c217affb1104b9abe3b17b
shimosyan <owner@cube-unit.net>
2025-08-16 00:49:25 +0900

    feat: まぐおつジェネレータのリダイレクトのためのCloudflareページルールの設定を追加 (#103)
    * feat: Cloudflareページルールの設定を追加
    
    - tfaction-root.yaml: Cloudflareページルール用の作業ディレクトリを追加
    - terraform/cloudflare/cube-unit.net/page-rules/import.tf: ページルールのインポート設定を追加
    - terraform/cloudflare/cube-unit.net/page-rules/locals.tf: ページルール用のローカル変数を追加
    - terraform/cloudflare/cube-unit.net/page-rules/main.tf: Terraform設定を追加
    - terraform/cloudflare/cube-unit.net/page-rules/page-rules.tf: Cloudflareページルールリソースを追加
    - terraform/cloudflare/cube-unit.net/page-rules/tfaction.yaml: 空のtfaction.yamlを追加
    
    * chore: update .terraform.lock.hcl
    
    * style: terraform fmt -recursive

---
+++ terraform/cloudflare/cube-unit.net/page-rules/main.tf
@@ -0,0 +1,23 @@
+/*
+  terraform 定義
+*/
+terraform {
+  required_version = ">= 1.5.0"
+
+  /*
+    .tfstate は AWS S3 に保存すること
+  */
+  backend "s3" {
+    bucket  = "terraform.cube-unit.net"
+    region  = "ap-northeast-1"
+    key     = "cube-unit.net/cloudflare/cube-unit.net/page-rules/terraform.tfstate"
+    encrypt = true
+  }
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5"
+    }
+  }
+}
