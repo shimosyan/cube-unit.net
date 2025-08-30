309aed30861d23b20bee9c0805ffa274425dadf9
shimosyan <owner@cube-unit.net>
2025-08-30 18:39:23 +0900

    feat(cube-unit.net/zero-trust)/update_zero-trust_applications (#131)
    * fix: 不要なインポート設定を削除
    
    terraform/cloudflare/cube-unit.net/zero-trust/import.tfファイルを削除しました。これにより、未使用のインポート設定が整理されました。
    
    * feat: Cloudflare Zero Trustアプリケーションの設定を統合
    
    - Staging環境とProxmoxサービスの設定をローカル変数に統合し、リソース定義を簡素化しました。
    - 各アプリケーションのURIとポリシーを明示的に定義しました。
    - 不要なリソース定義を削除し、リソースの移動を行いました。
    
    * style: terraform fmt -recursive

--- terraform/cloudflare/cube-unit.net/zero-trust/application.tf
+++ terraform/cloudflare/cube-unit.net/zero-trust/application.tf
@@ -1,31 +1,82 @@
-resource "cloudflare_zero_trust_access_application" "stg" {
+locals {
+  zero_trust_application_config = {
+    stg = {
+      name   = "Cube-Unit 公式サイト Staging環境"
+      domain = "stg.cube-unit.net"
+      type   = "self_hosted"
+      uris = [
+        "stg.cube-unit.net"
+      ]
+      policies = [
+        {
+          id         = cloudflare_zero_trust_access_policy.google_workspace_sso.id
+          precedence = 1
+        },
+      ]
+    },
+    proxmox = {
+      name   = "cube-unit Proxmox Service"
+      domain = "proxmox.cube-unit.net"
+      type   = "self_hosted"
+      uris = [
+        "proxmox.cube-unit.net",
+      ]
+      policies = [
+        {
+          id         = cloudflare_zero_trust_access_policy.github_actions_self_host_runner.id
+          precedence = 1
+        },
+        {
+          id         = cloudflare_zero_trust_access_policy.google_workspace_sso.id
+          precedence = 2
+        },
+      ]
+    },
+    internal = {
+      name   = "Cube-Unit Internal Services"
+      domain = "nas.cube-unit.net"
+      type   = "self_hosted"
+      uris = [
+        "nas.cube-unit.net",
+        "vscode.cube-unit.net",
+        "grafana.cube-unit.net",
+      ]
+      policies = [
+        {
+          id         = cloudflare_zero_trust_access_policy.google_workspace_sso.id
+          precedence = 1
+        },
+      ]
+    }
+  }
+}
+
+resource "cloudflare_zero_trust_access_application" "main" {
+  for_each = local.zero_trust_application_config
+
   account_id = local.account_id
-  name       = "stg.cube-unit.net"
-  domain     = "stg.cube-unit.net"
-  type       = "self_hosted"
+  name       = each.value.name
+  domain     = each.value.domain
+  type       = each.value.type
 
   allowed_idps = [
     local.google_workspace_sso_id
   ]
 
   destinations = [
+    for url in each.value.uris :
     {
       cidr        = null
       hostname    = null
       l4_protocol = null
       port_range  = null
       type        = "public"
-      uri         = "stg.cube-unit.net"
+      uri         = url
       vnet_id     = null
-    },
+    }
   ]
 
-  policies = [
-    {
-      id         = cloudflare_zero_trust_access_policy.google_workspace_sso.id
-      precedence = 1
-    },
-  ]
+  policies = each.value.policies
 
   allow_authenticate_via_warp     = null
   allow_iframe                    = null
@@ -59,95 +110,12 @@ resource "cloudflare_zero_trust_access_application" "stg" {
   zone_id                         = null
 }
 
+moved {
+  from = cloudflare_zero_trust_access_application.stg
+  to   = cloudflare_zero_trust_access_application.main["stg"]
+}
 
-resource "cloudflare_zero_trust_access_application" "proxmox" {
-  account_id = local.account_id
-  name       = "cube-unit Internal Service"
-  domain     = "proxmox.cube-unit.net"
-  type       = "self_hosted"
-
-  allowed_idps = [
-    local.google_workspace_sso_id
-  ]
-
-  destinations = [
-    {
-      cidr        = null
-      hostname    = null
-      l4_protocol = null
-      port_range  = null
-      type        = "public"
-      uri         = "proxmox.cube-unit.net"
-      vnet_id     = null
-    },
-    {
-      cidr        = null
-      hostname    = null
-      l4_protocol = null
-      port_range  = null
-      type        = "public"
-      uri         = "nas.cube-unit.net"
-      vnet_id     = null
-    },
-    {
-      cidr        = null
-      hostname    = null
-      l4_protocol = null
-      port_range  = null
-      type        = "public"
-      uri         = "vscode.cube-unit.net"
-      vnet_id     = null
-    },
-    {
-      cidr        = null
-      hostname    = null
-      l4_protocol = null
-      port_range  = null
-      type        = "public"
-      uri         = "grafana.cube-unit.net"
-      vnet_id     = null
-    },
-  ]
-
-  policies = [
-    {
-      id         = cloudflare_zero_trust_access_policy.github_actions_self_host_runner.id
-      precedence = 1
-    },
-    {
-      id         = cloudflare_zero_trust_access_policy.google_workspace_sso.id
-      precedence = 2
-    },
-  ]
-
-  allow_authenticate_via_warp     = null
-  allow_iframe                    = null
-  app_launcher_logo_url           = null
-  app_launcher_visible            = true
-  auto_redirect_to_identity       = false
-  bg_color                        = null
-  cors_headers                    = null
-  custom_deny_message             = null
-  custom_deny_url                 = null
-  custom_non_identity_deny_url    = null
-  custom_pages                    = null
-  enable_binding_cookie           = false
-  footer_links                    = null
-  header_bg_color                 = null
-  http_only_cookie_attribute      = true
-  landing_page_design             = null
-  logo_url                        = null
-  options_preflight_bypass        = false
-  path_cookie_attribute           = null
-  read_service_tokens_from_header = null
-  saas_app                        = null
-  same_site_cookie_attribute      = null
-  scim_config                     = null
-  service_auth_401_redirect       = null
-  session_duration                = "24h"
-  skip_app_launcher_login_page    = null
-  skip_interstitial               = null
-  tags                            = []
-  target_criteria                 = null
-  zone_id                         = null
+moved {
+  from = cloudflare_zero_trust_access_application.proxmox
+  to   = cloudflare_zero_trust_access_application.main["proxmox"]
 }
--- terraform/cloudflare/cube-unit.net/zero-trust/import.tf
+++
@@ -1,4 +0,0 @@
-import {
-  id = local.account_id
-  to = cloudflare_zero_trust_organization.main
-}
